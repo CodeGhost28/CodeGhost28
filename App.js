a21d3c9da90e42c7713bc502b164b3485a4c43f0
CodeGhost28 <komananiinstallations@gmail.com>
2024-10-08 22:55:48 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+import SignalDashboard from './components/SignalDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <SignalDashboard />
+    </div>
+  );
+}
+
+export default App;
